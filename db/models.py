6c3f1923357c256a0347062d9c3184f159b5508d
anonymousgirl123 <kaminikamaliem@gmail.com>
2026-03-25 20:40:18 +0530

    add DB in service

---
+++ db/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, String, Integer, Boolean
+from .database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(String, primary_key=True)
+    veg = Column(Boolean)
+    preferred_cuisine = Column(String)
+
+
+class FoodItem(Base):
+    __tablename__ = "food_items"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    cuisine = Column(String)
+    price = Column(Integer)
+    type = Column(String)
+    
